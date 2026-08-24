123cbfe820e77e5d524a703617ea96c2c4e18d59
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-23 20:50:30 -0600

    feat: project update

--- src/PCH.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include "F4SE/F4SE.h"
-#include "RE/Fallout.h"
-
-#include <glaze/glaze.hpp>
-
-using namespace std::literals;
--- xmake.lua
+++ xmake.lua
@@ -1,35 +1,23 @@
--- set minimum xmake version
-set_xmakever("2.9.4")
-
--- includes
+-- include subprojects
 includes("lib/commonlibf4")
 
--- set project
+-- set project constants
 set_project("BakaFullscreenPipboy")
 set_version("2.1.0")
 set_license("GPL-3.0")
-
--- set defaults
 set_languages("c++23")
 set_warnings("allextra")
 
--- set policies
-set_policy("package.requires_lock", true)
-
--- add rules
+-- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- set config
+-- set configs
 set_config("commonlib_ini", true)
 set_config("commonlib_json", true)
 
--- targets
+-- define targets
 target("BakaFullscreenPipboy")
-    -- bind local dependencies
-    add_deps("commonlibf4")
-
-    -- add commonlibf4 plugin
     add_rules("commonlibf4.plugin", {
         name = "BakaFullscreenPipboy",
         author = "shad0wshayd3"
